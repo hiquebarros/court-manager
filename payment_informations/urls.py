114d7bbc1bdc88d665d6319e9d703560cdd39abb
danielkleira <daniel_leira@hotmail.com>
2022-09-11 21:17:56 -0300

    fix/payment_return_ok

--- payment_informations/urls.py
+++ payment_informations/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('users/<pk>/payment/', views.PaymentInformationView.as_view()),
+    path('users/<user_id>/payment/<pk>', views.PaymentInformationIDView.as_view()),
 ]
--- payment_informations/views.py
+++ payment_informations/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-
+from rest_framework.authentication import TokenAuthentication
 from .models import Payment_information
 from .serializers import PaymentInformationSerializer
 from .permissions import IsAuthenticated, IsOwner
@@ -7,12 +7,24 @@ from .permissions import IsAuthenticated, IsOwner
 
 
 
-class PaymentInformationView(generics.ListCreateAPIView, generics.RetrieveDestroyAPIView):
-        permission_classes = [IsAuthenticated, IsOwner] 
-        queryset= Payment_information.objects.all()
-        serializer_class = PaymentInformationSerializer
-        
-        def perform_create(self, serializer):
-                serializer.save(user=self.request.user)
+
+class PaymentInformationView(generics.ListCreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsOwner] 
+    queryset= Payment_information.objects.all()
+    serializer_class = PaymentInformationSerializer
+    
+    import ipdb
+    ipdb.set_trace()
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+     
+
+
+class PaymentInformationIDView(generics.RetrieveDestroyAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsOwner]
+    queryset= Payment_information.objects.all()
+    serializer_class = PaymentInformationSerializer              
                 
                 
